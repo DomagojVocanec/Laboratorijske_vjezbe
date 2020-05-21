64c0a86f72fddd7dd5c209313f4b27c26638e66e
DomagojVocanec <65723945+DomagojVocanec@users.noreply.github.com>
2020-05-21 18:16:06 +0200

    Add files via upload

---
+++ LV2/LV2/Die.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV2
+{
+    class Die
+    {
+        private int numberOfSides;
+        private RandomGenerator random;
+        /*private Random randomGenerator;*/
+
+        /*public Die(int numberOfSides, Random random)
+        {
+            this.numberOfSides = numberOfSides;
+            this.randomGenerator = random;
+        }*/
+        public Die(int numberOfSides)
+        {
+            this.numberOfSides = numberOfSides;
+            /*this.randomGenerator = new Random();*/
+            this.random = RandomGenerator.GetInstance();
+        }
+        public int Roll()
+        {
+            /*int rolledNumber = randomGenerator.Next(1, numberOfSides + 1);*/
+            int rolledNumber = random.NextInt(1, numberOfSides + 1);
+            return rolledNumber;
+        }
+    }
+}
---
+++ LV2/LV2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dices = 20;
+
+            DiceRoller roller = new DiceRoller();
+            for (int i = 0; i < dices; i++)
+                roller.InsertDie(new Die(8));
+
+            roller.RollAllDice();
+            
+            roller.GetRollingResults();
+            for (int i = 0; i < roller.GetRollingResults().Count; i++)
+                Console.WriteLine(roller.GetRollingResults().ElementAt(i));
+        }
+    }
+}
